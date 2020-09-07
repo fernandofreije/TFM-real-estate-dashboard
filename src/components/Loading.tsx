2fd020455dd010c49bc2919591676ec6fe64a85b
fernandofreije <ffreijefuente@gmail.com>
2020-09-07 15:08:17 +0200

    Change loading color

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,5 +2,5 @@ import Loader from 'react-loader-spinner';
 import { ReactElement } from 'react';
 
 export default function Loading(): ReactElement {
-  return <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={999} />;
+  return <Loader type="Puff" color="#8884d8" height={100} width={100} timeout={999} />;
 }
